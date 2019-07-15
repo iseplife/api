718ea0ae8d5bf407488c123a1101bef12ae78d84
Paul <venard.paul@gmail.com>
2019-07-15 14:07:32 +0200

    feat(author): prepare author removal

--- src/main/java/com/iseplive/api/entity/user/Author.java
+++ src/main/java/com/iseplive/api/entity/user/Author.java
@@ -6,29 +6,7 @@ import javax.persistence.*;
  * Created by Guillaume on 03/08/2017.
  * back
  */
-@Entity
-@DiscriminatorColumn(name = "authorType")
+
 public abstract class Author {
-  @Id
-  @GeneratedValue
-  private Long id;
 
-  private boolean archived = false;
-
-  @Column(updatable = false, insertable = false)
-  private String authorType;
-
-
-
-  public String getAuthorType() {
-    return authorType;
-  }
-
-  public boolean isArchived() {
-    return archived;
-  }
-
-  public void setArchived(boolean archived) {
-    this.archived = archived;
-  }
 }
