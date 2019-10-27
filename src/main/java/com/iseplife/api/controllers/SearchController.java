21733fa66be25ac954105aee79c69427f82768e8
Paul <venard.paul@gmail.com>
2019-10-27 12:55:53 +0100

    feat(search): complete endpoints

--- src/main/java/com/iseplife/api/controllers/SearchController.java
+++ src/main/java/com/iseplife/api/controllers/SearchController.java
@@ -1,33 +1,41 @@
 package com.iseplife.api.controllers;
 
 
+import com.iseplife.api.dto.view.SearchItemView;
+import com.iseplife.api.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/search")
 public class SearchController {
 
-  @GetMapping
-  public void globalSearch(@RequestParam String filter){
+  @Autowired
+  SearchService searchService;
 
+  @GetMapping
+  public List<SearchItemView> globalSearch(@RequestParam String filter){
+    return searchService.globalSearch(filter);
   }
 
   @GetMapping("/user")
-  public void userSearch(@RequestParam String filter){
-
+  public List<SearchItemView> userSearch(@RequestParam String filter){
+    return searchService.searchUser(filter);
   }
 
   @GetMapping("/club")
-  public void clubSearch(@RequestParam String filter){
-
+  public List<SearchItemView> clubSearch(@RequestParam String filter){
+    return searchService.searchClub(filter);
   }
 
   @GetMapping("/event")
-  public void eventSearch(@RequestParam String filter){
-
+  public List<SearchItemView> eventSearch(@RequestParam String filter){
+    return searchService.searchEvent(filter);
   }
 
 }
