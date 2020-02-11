10b1b09635fc78d886c19db975d9846c2d281e0f
Bastien <basgrignon@gmail.com>
2020-02-11 14:23:46 +0100

    Add missing roles management in Controllers

--- src/main/java/com/iseplife/api/controllers/SearchController.java
+++ src/main/java/com/iseplife/api/controllers/SearchController.java
@@ -1,6 +1,7 @@
 package com.iseplife.api.controllers;
 
 
+import com.iseplife.api.constants.Roles;
 import com.iseplife.api.dto.view.SearchItemView;
 import com.iseplife.api.services.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -19,21 +21,25 @@ public class SearchController {
   SearchService searchService;
 
   @GetMapping
+  @RolesAllowed({Roles.STUDENT})
   public List<SearchItemView> globalSearch(@RequestParam String filter, @RequestParam(defaultValue = "0") Boolean allAnswer){
     return searchService.globalSearch(filter, allAnswer);
   }
 
   @GetMapping("/user")
+  @RolesAllowed({Roles.STUDENT})
   public List<SearchItemView> userSearch(@RequestParam String filter, @RequestParam(defaultValue = "0") Boolean allAnswer){
     return searchService.searchUser(filter, allAnswer);
   }
 
   @GetMapping("/club")
+  @RolesAllowed({Roles.STUDENT})
   public List<SearchItemView> clubSearch(@RequestParam String filter, @RequestParam(defaultValue = "0") Boolean allAnswer){
     return searchService.searchClub(filter, allAnswer);
   }
 
   @GetMapping("/event")
+  @RolesAllowed({Roles.STUDENT})
   public List<SearchItemView> eventSearch(@RequestParam String filter, @RequestParam(defaultValue = "0") Boolean allAnswer){
     return searchService.searchEvent(filter, allAnswer);
   }
