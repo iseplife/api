04e45c4c5236a45ef192128798d1af5b0f305b3f
Bastien <basgrignon@gmail.com>
2020-06-28 21:02:22 +0200

    feat(searchBar): Add type of entity to SearchItem

--- src/main/java/com/iseplife/api/dao/SearchFactory.java
+++ src/main/java/com/iseplife/api/dao/SearchFactory.java
@@ -1,5 +1,6 @@
 package com.iseplife.api.dao;
 
+import com.iseplife.api.constants.SearchItem;
 import com.iseplife.api.dto.view.SearchItemView;
 import com.iseplife.api.entity.club.Club;
 import com.iseplife.api.entity.event.Event;
@@ -12,11 +13,13 @@ import java.util.Date;
 @Component
 public class SearchFactory {
 
-  public SearchItemView entityToSearchItemView(Event event){
+  public SearchItemView entityToSearchItemView(Event event) {
     SearchItemView searchItem = new SearchItemView();
     searchItem.setId(event.getId());
     searchItem.setName(event.getTitle());
     searchItem.setThumbURL(event.getImageUrl());
+    searchItem.setDescription(event.getDescription());
+    searchItem.setType(SearchItem.EVENT);
 
     // Status is false when event is passed
     searchItem.setStatus(!event.getEndsAt().before(new Date()));
@@ -28,6 +31,7 @@ public class SearchFactory {
     searchItem.setId(club.getId());
     searchItem.setName(club.getName());
     searchItem.setThumbURL(club.getLogoUrl());
+    searchItem.setType(SearchItem.CLUB);
 
     // Status is false when club is archived
     searchItem.setStatus(!club.isArchived());
@@ -40,6 +44,7 @@ public class SearchFactory {
     searchItem.setName(student.getFirstName() + " " + student.getLastName());
     searchItem.setThumbURL(student.getPicture());
     searchItem.setDescription(student.getPromo().toString());
+    searchItem.setType(SearchItem.STUDENT);
 
     // Status is false when student is archived or promotion is passed
     searchItem.setStatus(!student.isArchived() || student.getPromo() < Calendar.getInstance().get(Calendar.YEAR));
