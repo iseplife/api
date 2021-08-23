55fabd02da45b79716702ff961776f92c9d89cab
Paul Vénard <pvenard@juniorisep.com>
2021-08-23 18:49:43 +0200

    feat(comment): create thread trending comments query

--- src/main/java/com/iseplife/api/dao/post/CommentRepository.java
+++ src/main/java/com/iseplife/api/dao/post/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.iseplife.api.dao.post;
 
+import com.iseplife.api.entity.Thread;
 import com.iseplife.api.entity.post.Comment;
 import com.iseplife.api.entity.post.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Created by Guillaume on 31/07/2017.
@@ -11,4 +17,10 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+  @Query(
+    "select c from Comment c where c.parentThread.id = ?1 " +
+      "order by c.thread.likes.size desc "
+  )
+  Page<Comment> findTrendingComments(Long thread, Pageable pageable);
 }
