50684bc14b46496e9732521979c38bb6269b911c
Paul Vénard <pvenard@juniorisep.com>
2021-09-21 19:38:17 -0400

    feat(comment): add thread to CommentFormView

--- src/main/java/com/iseplife/api/dao/post/CommentFactory.java
+++ src/main/java/com/iseplife/api/dao/post/CommentFactory.java
@@ -32,14 +32,16 @@ public class CommentFactory {
   }
 
   public CommentFormView toView(Comment comment) {
-//    mapper.typeMap(Comment.class, CommentFormView.class).addMappings(mapper -> {
-//      mapper
-//        .using(ctx -> AuthorFactory.toView((Club) ctx.getSource())).
-//        .when(ctx -> ctx.getSource() != null)
-//        .map(Comment::getAsClub, CommentFormView::setAuthor
-//      );
-//    });
+    mapper.typeMap(Comment.class, CommentFormView.class).addMappings(mapper -> {
+      mapper.map(src -> src.getThread().getId(), CommentFormView::setThread);
+      mapper.skip(CommentFormView::setAuthor);
+    });
+    CommentFormView view = mapper.map(comment, CommentFormView.class);
+    view.setAuthor(comment.getAsClub() != null ?
+      AuthorFactory.toView(comment.getAsClub()) :
+      AuthorFactory.toView(comment.getStudent())
+    );
 
-    return mapper.map(comment, CommentFormView.class);
+    return view;
   }
 }
