f665b3bd43546b4d4c4de66d7270a249a194d35c
Léo MELKI <melkileo@gmail.com>
2021-11-09 11:29:37 +0100

    fix(subscription): sub repo's deleteBySubscribedIdAndListenerId fixed

--- src/main/java/com/iseplife/api/dao/feed/SubscriptionRepository.java
+++ src/main/java/com/iseplife/api/dao/feed/SubscriptionRepository.java
@@ -1,8 +1,10 @@
 package com.iseplife.api.dao.feed;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.iseplife.api.entity.subscription.Subscription;
 
@@ -17,6 +19,8 @@ public interface SubscriptionRepository extends CrudRepository<Subscription, Lon
     "s.listener.id = ?2 and s.subscribed.id = ?1")
   Subscription findBySubscribedIdAndListenerId(Long id, Long listenerID);
 
+  @Transactional
+  @Modifying
   @Query("delete from Subscription s where " +
     "s.listener.id = ?2 and s.subscribed.id = ?1")
   void deleteBySubscribedIdAndListenerId(Long id, Long listenerID);
