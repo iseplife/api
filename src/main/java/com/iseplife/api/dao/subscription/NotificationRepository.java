80e001554d417b2262d7b5def4860474a7e74d7a
Léo MELKI <melkileo@gmail.com>
2021-11-12 15:50:07 +0100

    feat(notification): save notifications

--- src/main/java/com/iseplife/api/dao/subscription/NotificationRepository.java
+++ src/main/java/com/iseplife/api/dao/subscription/NotificationRepository.java
@@ -1,4 +1,10 @@
 package com.iseplife.api.dao.subscription;
 
-public interface NotificationRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iseplife.api.entity.subscription.Notification;
+
+@Repository
+public interface NotificationRepository extends CrudRepository<Notification, Long> {
 }
--- src/main/java/com/iseplife/api/services/NotificationService.java
+++ src/main/java/com/iseplife/api/services/NotificationService.java
@@ -1,16 +1,21 @@
 package com.iseplife.api.services;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.springframework.stereotype.Service;
 
+import com.iseplife.api.dao.subscription.NotificationRepository;
 import com.iseplife.api.dao.subscription.SubscriptionRepository;
 import com.iseplife.api.entity.subscription.Notification;
 import com.iseplife.api.entity.subscription.Subscribable;
 import com.iseplife.api.entity.subscription.Subscription;
+import com.iseplife.api.entity.user.Student;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NotificationService {
   private final SubscriptionRepository subscriptionRepository;
+  private final NotificationRepository notificationRepository;
   private final WebPushService webPushService;
   
   public void delayNotification(Notification notif, boolean extensive, Subscribable subable, DelayedNotificationCheck check) {
@@ -28,9 +34,17 @@ public class NotificationService {
       @Override
       public void run() {
         if(check.isNotificationStillUseful()) {
+          Set<Student> notified = new HashSet<>();
           List<Subscription> subs = subscriptionRepository.findBySubscribed(subable);
           if(extensive)
             subs = Arrays.asList(subs.stream().filter(sub -> sub.isExtensiveSubscription()).toArray(i->new Subscription[i]));
+          
+          subs.forEach(s -> notified.add(s.getListener()));
+          
+          notif.setStudents(new ArrayList<Student>(notified));
+          
+          notificationRepository.save(notif);
+          
           webPushService.sendNotificationToAll(subs, notif.getPayload());
         }
       }
