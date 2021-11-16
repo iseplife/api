a197ec7a1dba88adebedf849ae6df7afa762ec14
Léo MELKI <leme60926@eleve.isep.fr>
2021-11-16 20:03:27 +0100

    refactor(subscription): tey catch and use exception instead of throws

--- src/main/java/com/iseplife/api/controllers/WebPushController.java
+++ src/main/java/com/iseplife/api/controllers/WebPushController.java
@@ -8,10 +8,12 @@ import java.util.concurrent.TimeoutException;
 import javax.annotation.security.RolesAllowed;
 
 import org.jose4j.lang.JoseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.github.lambdaexpression.annotation.EnableRequestBodyParam;
 import com.github.lambdaexpression.annotation.RequestBodyParam;
@@ -31,8 +33,12 @@ public class WebPushController {
   
   @PostMapping("/register/init")
   @RolesAllowed({ Roles.STUDENT })
-  public void registerPushService(@RequestBody RegisterPushServiceDTO register) throws GeneralSecurityException, IOException, JoseException, ExecutionException, InterruptedException, TimeoutException {
-    webpushService.registerWebPushService(register);
+  public void registerPushService(@RequestBody RegisterPushServiceDTO register) {
+    try {
+      webpushService.registerWebPushService(register);
+    }catch(Exception e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something wrong happened when registering your push service");
+    }
   }
   @PostMapping("/register/validate")
   public void validatePushService(@RequestBodyParam String key) {
