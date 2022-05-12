16a36748feb1f5c606b7ff2990b151f4df40e6c2
Paul <dev.warthy@gmail.com>
2022-05-12 21:44:21 +0200

    fix(post): send PostView in WSPSFeedPostCreated

--- src/main/java/com/iseplife/api/dao/post/PostFactory.java
+++ src/main/java/com/iseplife/api/dao/post/PostFactory.java
@@ -2,6 +2,8 @@ package com.iseplife.api.dao.post;
 
 import javax.annotation.PostConstruct;
 
+import com.iseplife.api.dto.post.view.PostContextView;
+import com.iseplife.api.entity.feed.Feed;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -22,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class PostFactory {
   @Lazy final private EmbedFactory embedFactory;
   final private ModelMapper mapper;
-  
+
   @PostConstruct
   public void init() {
     mapper.typeMap(PostProjection.class, PostView.class)
@@ -35,7 +37,21 @@ public class PostFactory {
           .using(ctx -> embedFactory.toView((Embedable) ctx.getSource()))
           .map(PostProjection::getEmbed, PostView::setEmbed);
       });
-    
+
+    mapper.typeMap(Post.class, PostView.class)
+      .addMappings(mapper -> {
+        mapper.skip(PostView::setLiked);
+        mapper
+          .using(ctx -> SecurityService.hasRightOn((PostProjection) ctx.getSource()))
+          .map(src -> src, PostView::setHasWriteAccess);
+        mapper
+          .using(ctx -> new PostContextView((Feed) ctx.getSource()))
+          .map(Post::getFeed, PostView::setContext);
+        mapper
+          .using(ctx -> embedFactory.toView((Embedable) ctx.getSource()))
+          .map(Post::getEmbed, PostView::setEmbed);
+      });
+
     mapper.typeMap(Post.class, PostFormView.class)
       .addMappings(mapper -> {
         mapper
@@ -54,6 +70,14 @@ public class PostFactory {
   }
 
   public PostView toView(PostProjection post, Boolean isLiked, CommentView trendingComment) {
+    PostView view = mapper.map(post, PostView.class);
+    view.setLiked(isLiked);
+    view.setTrendingComment(trendingComment);
+
+    return view;
+  }
+
+  public PostView toView(Post post, Boolean isLiked, CommentView trendingComment) {
     PostView view = mapper.map(post, PostView.class);
     view.setLiked(isLiked);
     view.setTrendingComment(trendingComment);
---
+++ src/main/java/com/iseplife/api/dto/post/view/PostContextView.java
@@ -0,0 +1,17 @@
+package com.iseplife.api.dto.post.view;
+
+import com.iseplife.api.constants.FeedType;
+import com.iseplife.api.dao.post.projection.PostContextProjection;
+import com.iseplife.api.entity.feed.Feed;
+import lombok.Data;
+
+@Data
+public class PostContextView implements PostContextProjection {
+  private Long id;
+  private FeedType type;
+
+  public PostContextView(Feed feed){
+    id = feed.getId();
+    type = feed.getType();
+  }
+}
