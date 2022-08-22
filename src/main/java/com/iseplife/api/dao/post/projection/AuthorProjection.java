b25cc8c1b7462e32d41a4fb2f1702c5ccf8a13c1
leomelki <melkileo@gmail.com>
2022-08-22 22:18:37 +0200

    fix(cache): add feed id and some fixes for cache

--- src/main/java/com/iseplife/api/dao/post/projection/AuthorProjection.java
+++ src/main/java/com/iseplife/api/dao/post/projection/AuthorProjection.java
@@ -1,10 +1,16 @@
 package com.iseplife.api.dao.post.projection;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.iseplife.api.constants.AuthorType;
 
 public interface AuthorProjection {
   Long getId();
   String getName();
+  @Value("#{target.feed.id}")
+  Long getFeedId();
   AuthorType getAuthorType();
+  @Value("#{target instanceof T(com.iseplife.api.entity.club.Club) ? target.description : null}")
+  String getDescription();
   String getThumbnail();
 }
