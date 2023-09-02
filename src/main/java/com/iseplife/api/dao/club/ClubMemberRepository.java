7a2095f2aabff6d127df42dd6947737040c4c319
Clément Delestre <clementdelestre@gmail.com>
2023-09-02 15:31:38 +0200

    fix(clubs): return students clubs of the current school year

--- src/main/java/com/iseplife/api/dao/club/ClubMemberRepository.java
+++ src/main/java/com/iseplife/api/dao/club/ClubMemberRepository.java
@@ -30,9 +30,11 @@ public interface ClubMemberRepository extends CrudRepository<ClubMember, Long> {
 
   @Query(
     "select cm from ClubMember cm " +
-      "where cm.student.id = :student_id and cm.club.viewable = true"
+      "where cm.student.id = :student_id " +
+      "and cm.club.viewable = true " +
+      "and cm.fromYear <= :year and cm.toYear >= :year "
   )
-  List<ClubMemberStudentProjection> findByStudentId(Long student_id);
+  List<ClubMemberStudentProjection> findByStudentId(Long student_id, Integer year);
 
   @Query(
     "select count(cm.id) from ClubMember cm " +
