dc884ef4c6469af99d19ab1a7571b48924bf2396
Léo MELKI <melkileo@gmail.com>
2025-06-03 12:17:28 +0200

    test: add cas login logs

--- src/main/java/com/iseplife/api/services/CASService.java
+++ src/main/java/com/iseplife/api/services/CASService.java
@@ -120,6 +120,9 @@ public CASUserDTO identifyToCASSSO(String ticket, String service) {
     try {
       var db = dbf.newDocumentBuilder();
       var doc = db.parse(new ByteArrayInputStream(resp.getBytes()));
+
+      System.out.println(doc)
+      System.out.println(doc.getElementsByTagName("cas:prenom").item(0).getTextContent() + " connected using SSO")
       return CASUserDTO.builder()
         .numero(Long.valueOf(doc.getElementsByTagName("cas:numero").item(0).getTextContent()))
         .nom(doc.getElementsByTagName("cas:nom").item(0).getTextContent())
